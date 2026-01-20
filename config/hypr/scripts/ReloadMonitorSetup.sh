385402f3033081bb1fed5945a18181e44dae0fed
beispielsweise <egputilin@gmail.com>
2026-01-20 20:12:29 +0100

    The setup was a bit broken on my secondary machine with Windows dualboot. This is an attempt to fix it, however im allready regretting maintaining two machines... Turns out, a lot of stuff actually broke and fixing it takes a whole different approach. Note for future self: the current monitor handling system is too barebones, smart monitor and layout detection needs to be added in the future. This version is however quite stable on both systems, so this will do. Waybar breaks a bit, but I won't mess with it for now, whatever.

--- config/hypr/scripts/ReloadMonitorSetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if HDMI-A-1 is connected
-if hyprctl monitors | grep HDMI; then
-  hyprctl keyword monitor "eDP-1,preferred,0x0,1"
-  hyprctl keyword monitor "HDMI-A-1,preferred,1920x0,auto,mirror,eDP-1"
-else
-  hyprctl keyword monitor ",preferred,0x0,1"
-fi
